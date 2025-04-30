e9a9354a0890ba1d01fbdc843faad422aec94818
Risav Arrahman <risavbm@gmail.com>
2025-04-30 18:33:41 +0700

    added script to code

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const celsiusInput = document.getElementById("celsius");
+const fahrenheitInput = document.getElementById("fahrenheit");
+const calculationText = document.getElementById("calculation");
+
+// Fungsi konversi Celsius ke Fahrenheit
+function convertSuhu() {
+  const celsiusValue = parseFloat(celsiusInput.value);
+
+  // Cek apakah input adalah angka yang valid
+  if (isNaN(celsiusValue)) {
+    alert("Masukkan angka yang valid di Celsius!");
+    return; // jika input tidak valid, hentikan eksekusi
+  }
+
+  const fahrenheitValue = (celsiusValue * 9) / 5 + 32;
+  fahrenheitInput.value = fahrenheitValue.toFixed(2);
+  calculationText.value = `${celsiusValue}°C x (9/5) + 32 = ${fahrenheitValue.toFixed(2)}°F`;
+}
+
+// Fungsi reset semua input dan hasil
+function resetForm() {
+  console.log("reset button clicked");
+  celsiusInput.value = "";
+  fahrenheitInput.value = "";
+  calculationText.value = "";
+}
+
+function reverse() {
+  console.log("reverse button clicked");
+  const fahrenheitValue = parseFloat(fahrenheitInput.value);
+
+  if (isNaN(fahrenheitValue)) {
+    alert("Masukkan angka yang valid di Fahrenheit!");
+    return;
+  }
+
+  const celsiusValue = ((fahrenheitValue - 32) * 5) / 9;
+  celsiusInput.value = celsiusValue.toFixed(2);
+  calculationText.value = `${fahrenheitValue}°F - 32 x (5/9) = ${celsiusValue.toFixed(2)}°C`;
+}
